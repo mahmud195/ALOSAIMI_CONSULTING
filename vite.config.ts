033d3c81e5bfbeff84a42dc940ec19eebf759bfd
Mahmoud Abdelazem <83193905+mahmud195@users.noreply.github.com>
2026-01-06 14:43:57 +0200

    [[

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/remix-of-ai-video-studio-landing-page/",
+  base: "/ALOSAIMI_CONSULTING/",
   server: {
     host: "::",
     port: 8080,
